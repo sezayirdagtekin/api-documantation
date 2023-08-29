31e87cce759dd90a5682bc7aed912971401cafaa
Sezayir Dagtekin <sezayir.dagtekin@globallogic.com>
2023-08-29 16:29:50 +0200

    format

--- openapi/demo/src/main/java/com/sezo/config/OpenApiConfig.java
+++ openapi/demo/src/main/java/com/sezo/config/OpenApiConfig.java
@@ -7,20 +7,16 @@ import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
-		info =@Info( contact = @Contact(name = "Sezayir",email = "contactme@gmail.com", url = "https://springdoc.org/"),
-		description = "OpenApi documentation for Springboot"
-		,title = "Open Api Specification"
-		,version = "1.0"
-		,license = @License(name = "Licence @sezo" ,url = "http://somelicens.com")
-		,termsOfService = "Terms of service!"
-		)
-		,servers = {
+	   info = @Info(
+						contact = @Contact(name = "Sezayir", email = "contactme@gmail.com", url = "https://springdoc.org/"),
+						description = "OpenApi documentation for Springboot", title = "Open Api Specification", 
+						version = "1.0",
+						license = @License(name = "Licence @sezo", url = "http://somelicens.com"), 
+						termsOfService = "Terms of service!"), 
+	   servers = {
 				@Server(description = "local", url = "http://localhost:8080"),
-				@Server(description = "dev", url = "http://dev:8080"),
-		}
-		
-		
-		)
+				@Server(description = "dev", url = "http://dev:8080"), 
+			  })
 public class OpenApiConfig {
 
 }
