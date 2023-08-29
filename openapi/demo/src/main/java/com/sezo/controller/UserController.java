7e7d9acc34450dc46e6af4a0226bafcf268f32ce
Sezayir Dagtekin <sezayir.dagtekin@globallogic.com>
2023-08-29 17:41:14 +0200

    response annotioons

--- openapi/demo/src/main/java/com/sezo/controller/UserController.java
+++ openapi/demo/src/main/java/com/sezo/controller/UserController.java
@@ -13,8 +13,13 @@ import com.sezo.entity.User;
 import com.sezo.service.UserService;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.models.media.MediaType;
 
 @RequestMapping("/user")
 @RestController
@@ -40,7 +45,9 @@ public class UserController {
 
 
 	@GetMapping("/greeting/{id}")
-	@Operation(description = "Save single user", summary = "Save user")
+	@Operation(description = "Save single user", summary = "Save user" ,parameters = {@Parameter(name = "id" ,description = "User id", example = "1")},
+			 responses = {@ApiResponse(responseCode ="200", content = @Content(schema = @Schema( implementation = User.class) , mediaType = org.springframework.http.MediaType.APPLICATION_JSON_VALUE))}
+			)
 	public User getUser(  @PathVariable Long	 id) {
 		return userService.getUser(id).orElse(new User(0L, "Default", "User"));
 	}
