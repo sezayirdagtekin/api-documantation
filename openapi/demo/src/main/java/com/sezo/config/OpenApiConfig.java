d42a83086d3301328d1d1e3eaecc595c68254c2f
Sezayir Dagtekin <sezayir.dagtekin@globallogic.com>
2023-08-29 13:42:19 +0200

    openapi config classs

---
+++ openapi/demo/src/main/java/com/sezo/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.sezo.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+		info =@Info( contact = @Contact(name = "Sezayir",email = "contactme@gmail.com", url = "https://springdoc.org/"),
+		description = "OpenApi documentation for Springboot"
+		,title = "Open Api Specification"
+		,version = "1.0"
+		,license = @License(name = "Licence @sezo" ,url = "http://somelicens.com")
+		,termsOfService = "Terms of service!"
+		)
+		,servers = {
+				@Server(description = "local", url = "http://localhost:8080"),
+				@Server(description = "dev", url = "http://dev:8080"),
+		}
+		
+		
+		)
+public class OpenApiConfig {
+
+}
